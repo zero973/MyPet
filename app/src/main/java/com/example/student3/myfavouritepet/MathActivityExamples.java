0f626b5de3855b0e4f5fae0d765faea050e80483
zero973 <lion_lusik@bk.ru>
2017-02-26 11:31:59 +0400

    HomeWork

---
+++ app/src/main/java/com/example/student3/myfavouritepet/MathActivityExamples.java
@@ -0,0 +1,172 @@
+package com.example.student3.myfavouritepet;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.Random;
+
+public class MathActivityExamples extends AppCompatActivity {
+    byte[] znakMass = new byte[2];
+    int[] numsMass = new int[4];
+    int[] rightAnswers = new int[2];
+    int[] userAnswers = new int[2];
+    byte k = 0, i = 0, j = 0, userLevel = 1;
+    int countRight = 0;
+    int count = 0;
+
+    Button nextbutton;
+    Button checkBut;
+    TextView t1;
+    TextView t2;
+    TextView t3;
+    TextView t4;
+    TextView t5;//znak
+    TextView t6;//znak
+    TextView TextResult;
+    EditText ans1;
+    EditText ans2;
+    Random rand = new Random();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.math_activity_examples);
+        checkBut = (Button)findViewById(R.id.buttonCheckAnswers);
+        t1 = (TextView)findViewById(R.id.TextFieldChislo1);
+        t2 = (TextView)findViewById(R.id.TextFieldChislo2);
+        t3 = (TextView)findViewById(R.id.TextFieldChislo3);
+        t4 = (TextView)findViewById(R.id.TextFieldChislo4);
+        t5 = (TextView)findViewById(R.id.TextZnak1);
+        t6 = (TextView)findViewById(R.id.TextZnak2);
+        TextResult = (TextView)findViewById(R.id.textViewRightAnswers);
+        ans1 = (EditText)findViewById(R.id.Answer1);
+        ans2 = (EditText)findViewById(R.id.Answer2);
+        ChangeSymbolOnLabel();
+        ChangeChisloOnLabel();
+
+        checkBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CheckResults();
+                if (countRight == 5 || countRight == 6)
+                    userLevel++;
+                else if (countRight == 15 || countRight == 16)
+                    userLevel++;
+                else if (countRight == 25 || countRight == 26)
+                    userLevel++;
+                if (userLevel == 4){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MathActivityExamples.this);
+                    builder.setTitle("Игра окончена!")
+                            .setMessage("Правильных ответов: " + countRight + " из " + count)
+                            .setCancelable(false)
+                            .setNegativeButton("Закончить игру",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int id) {
+                                            dialog.cancel();
+                                            finish();
+                                        }
+                                    });
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                }
+                ChangeSymbolOnLabel();
+                ChangeChisloOnLabel();
+                ans1.setText("0");
+                ans2.setText("0");
+            }
+        });
+    }
+
+    void CheckResults(){
+        k = 0;
+        for (int i = 0; i < 4; i += 2){
+            Chitatel(numsMass[i], numsMass[i+1], znakMass[k]);
+            k++;
+        }//задали ответы
+        try {
+            userAnswers[0] = Integer.valueOf(ans1.getText().toString());
+            userAnswers[1] = Integer.valueOf(ans2.getText().toString());//ответы юзера
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(), "Не вводите буквы и другие символы", Toast.LENGTH_LONG).show();
+            userAnswers[0] = userAnswers[1] = -2287777;
+        }
+        count += 2;
+        for (int i = 0; i < 2; i++){
+            if (userAnswers[0] == 228777){
+                userLevel = 3;
+                countRight += 25;
+                break;
+            }
+            if (userAnswers[i] == rightAnswers[i])
+                countRight++;
+        }
+        TextResult.setText("Правильных ответов " + countRight + " из " + count);
+        k = 0; i = 0; j = 0;
+    }
+
+    int GenerateNum(){
+        short diapozon = 0;
+        if (userLevel == 1)
+            diapozon = 50;
+        else if(userLevel == 2)
+            diapozon = 500;
+        else if (userLevel >= 3 && znakMass[k-1] != 2)
+            diapozon = 1000;
+        else if (userLevel >= 3 && znakMass[k-1] == 2)
+            diapozon = 50;
+        int chislo = rand.nextInt(diapozon);
+        numsMass[i] = chislo;
+        i++;
+        return chislo;
+    }
+
+    String GenerateZnak(){
+        String symbol = "";
+        byte diapozon = 0;
+        if (userLevel <= 2)
+            diapozon = 2;
+        else if(userLevel > 2)
+            diapozon = 3;
+        byte index = (byte)rand.nextInt(diapozon);
+        switch (index)
+        {
+            case 0: symbol = "+"; break;
+            case 1: symbol = "―"; break;
+            case 2: symbol = "X"; break;
+        }
+        znakMass[k] = index;
+        k++;
+        return symbol;
+    }
+
+    void ChangeSymbolOnLabel(){
+        t5.setText(GenerateZnak());
+        t6.setText(GenerateZnak());
+    }
+
+    void ChangeChisloOnLabel(){
+        t1.setText(String.valueOf(GenerateNum()));
+        t2.setText(String.valueOf(GenerateNum()));
+        t3.setText(String.valueOf(GenerateNum()));
+        t4.setText(String.valueOf(GenerateNum()));
+    }
+
+    void Chitatel(int f1, int f2, byte s){
+        int result = 0;
+        switch (s)
+        {
+            case 0: result = f1 + f2; break;
+            case 1: result = f1 - f2; break;
+        }
+        rightAnswers[j] = result;
+        j++;
+    }
+}
---
+++ app/src/main/java/com/example/student3/myfavouritepet/RusActivity_Udarenie.java
@@ -0,0 +1,110 @@
+package com.example.student3.myfavouritepet;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class RusActivity_Udarenie extends AppCompatActivity {
+
+    byte countRightAnswers = 0, index = 0, numTrueCB, numUserCB;
+    Random random = new Random();
+    String[] answers = new String[4];
+    String[] mass = new String[4];
+
+    TextView tvQuestion, tvCountRight;
+    Button checkBut;
+    CheckBox cb1;
+    CheckBox cb2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rus_activity_udarenie);
+        tvQuestion = (TextView) findViewById(R.id.textViewQuestionRusUdarenie);
+        tvCountRight = (TextView)findViewById(R.id.textViewCountRightAnswersRusFirstLevelUdarenie);
+        checkBut = (Button) findViewById(R.id.buttonCheckRusUdarenie);
+        cb1 = (CheckBox) findViewById(R.id.checkBoxRus1Udarenie);
+        cb2 = (CheckBox) findViewById(R.id.checkBoxRus2Udarenie);
+
+        mass = getResources().getStringArray(R.array.rusQuestions_Udarenie);
+        answers = getResources().getStringArray(R.array.rusQuestions_Answers_Udarenie);
+
+        Play();
+
+        cb1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (cb2.isChecked() == true)
+                    cb2.setChecked(false);
+            }
+        });
+
+        cb2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (cb1.isChecked() == true)
+                    cb1.setChecked(false);
+            }
+        });
+
+        checkBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cb1.isChecked() == true)
+                    numUserCB = 0;
+                else {
+                    numUserCB = 1;
+                }
+                if (numTrueCB == numUserCB) {
+                    countRightAnswers++;
+                    tvCountRight.setText("Правильных ответов: " + countRightAnswers + " из 4");
+                    Play();
+                }
+                else {
+                    Play();
+                }
+            }
+        });
+    }
+
+    void Play(){
+        cb1.setChecked(false);
+        cb2.setChecked(false);
+        if (index == 4) {
+            index = 0;
+            AlertDialog.Builder builder = new AlertDialog.Builder(RusActivity_Udarenie.this);
+            builder.setTitle("Игра окончена!")
+                    .setMessage("Правильных ответов: " + countRightAnswers + " из 4")
+                    .setCancelable(false)
+                    .setNegativeButton("Закончить игру",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                    finish();
+                                }
+                            });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+        byte v = (byte) random.nextInt(2);//0 - первый чекбокс верный, 1 - первый чекбокс неверный
+        if (v == 0) {
+            cb1.setText(answers[index]);
+            cb2.setText(mass[index]);
+        }
+        else{
+            cb1.setText(mass[index]);
+            cb2.setText(answers[index]);
+        }
+        index++;
+        numTrueCB = v;
+    }
+}
