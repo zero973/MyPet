286c1361f275dce1faa1008cabb1731ed57b5200
zero973 <lion_lusik@bk.ru>
2017-03-29 18:17:05 +0400

    HomeWork

---
+++ app/src/main/java/com/example/student3/myfavouritepet/CaressActivity.java
@@ -0,0 +1,17 @@
+package com.example.student3.myfavouritepet;
+
+import android.app.Application;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class CaressActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new DrawView(this));
+    }
+}
